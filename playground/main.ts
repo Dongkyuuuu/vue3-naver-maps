9ce9bab601a043f5cec3506606c7ee1e6c38ec00
Dongkyuuuu <desktop@kakao.com>
2021-08-24 01:22:18 +0900

    chore: playground maps clinetId set

--- playground/main.ts
+++ playground/main.ts
@@ -2,6 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import navermaps from "../dist/vue3-naver-maps.js";
 
+/**
+ * create config.ts file
+ * export default "yout clientId"
+ */
+import clientId from "./config";
+
 const app = createApp(App);
 
-app.use(navermaps, { clientID: "c4ovrqy4fg" }).mount("#app");
+app.use(navermaps, { clientID: clientId }).mount("#app");
