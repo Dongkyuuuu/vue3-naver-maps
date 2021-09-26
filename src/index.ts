5542b2e1daee11933eacea94d78ddd2fc325499a
Dongkyuuuu <desktop@kakao.com>
2021-09-26 20:29:57 +0900

    feat(infowindow): register infowindow components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { install } from "./config";
 import NaverMaps from "./components/Map.vue";
 import NaverMarker from "./components/Marker.vue";
+import NaverInfoWindow from "./components/InfoWindow.vue";
 
 // export * from "./apis";
 export { naverV3 } from "./types";
@@ -8,6 +9,6 @@ export { naverV3 } from "./types";
 /**
  * Export Components
  */
-export { NaverMaps, NaverMarker };
+export { NaverMaps, NaverMarker, NaverInfoWindow };
 
 export default install;
