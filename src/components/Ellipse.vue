998f430c1765fa66a9f129ef56a2ac4d02b9dd62
dongkyuuuu <desktop@kakao.com>
2022-07-28 00:58:15 +0900

    chore(Ellipse): update ellipse logic

--- src/components/Ellipse.vue
+++ src/components/Ellipse.vue
@@ -1,63 +1,34 @@
-<template></template>
+<script lang="ts" setup>
+import { ref, inject, onMounted, onUnmounted, toRefs } from "vue";
+import { addEventEllipse } from "@/composables/useEvent";
+import { UI_EVENT_ELLIPSE } from "@/assets/event";
+import { MAPS_INSTANCE } from "@/config/keys";
 
-<script lang="ts">
-import {
-  defineComponent,
-  PropType,
-  inject,
-  ref,
-  toRefs,
-  onMounted,
-  onUnmounted,
-  watchEffect,
-} from "vue";
-import { naverMapObject } from "../injectionKeys";
-import { UI_EVENT_ELLIPSE, addEventEllipse } from "../utils";
+const props = defineProps<{
+  bounds: naver.maps.Bounds | naver.maps.BoundsLiteral;
+  options?: naver.maps.EllipseOptions;
+}>();
+const emits = defineEmits(["onLoad", ...UI_EVENT_ELLIPSE]);
 
-export default defineComponent({
-  name: "Ellipse",
-  emits: [...UI_EVENT_ELLIPSE, "onLoad"],
-  props: {
-    bounds: {
-      type: Object as PropType<naver.maps.Bounds | naver.maps.BoundsLiteral>,
-      required: true,
-    },
-    options: {
-      type: Object as PropType<naver.maps.EllipseOptions>,
-      default: {},
-    },
-  },
-  setup: (props, { emit }) => {
-    const map = inject(naverMapObject)!;
-    const ellipse = ref<naver.maps.Ellipse>();
-    const { bounds, options } = toRefs(props);
+const { bounds, options } = toRefs(props);
+const map = inject(MAPS_INSTANCE)!;
+const ellipse = ref<naver.maps.Ellipse>();
 
-    const createEllipse = () => {
-      ellipse.value = new window.naver.maps.Ellipse(
-        Object.assign(
-          {
-            map: map.value!,
-            bounds: bounds.value,
-          },
-          options.value
-        )
-      );
+const getEllipseInstance = () => {
+  const ellipseOption = {
+    map: map.value,
+    bounds: bounds.value,
+  };
+  ellipse.value = new naver.maps.Ellipse(
+    Object.assign(ellipseOption, options?.value ?? {})
+  );
 
-      addEventEllipse(emit, ellipse.value);
-      emit("onLoad", ellipse.value);
-    };
-    const setEllipse = (newOptions: naver.maps.EllipseOptions) => {
-      ellipse.value!.setOptions(newOptions);
-    };
+  addEventEllipse(emits, ellipse.value);
+  emits("onLoad", ellipse.value);
+};
 
-    onMounted(() => {
-      watchEffect(() => {
-        const newOptions = options.value;
-        if (!map.value) return;
-        ellipse.value ? setEllipse(newOptions) : createEllipse();
-      });
-    });
-    onUnmounted(() => ellipse.value!.setMap(null));
-  },
-});
+onMounted(() => getEllipseInstance());
+onUnmounted(() => ellipse.value!.setMap(null));
 </script>
+
+<template></template>
