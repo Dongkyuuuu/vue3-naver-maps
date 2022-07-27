44bae9261ca14cacbf272cf94a59cf1b5aa75b1a
dongkyuuuu <desktop@kakao.com>
2022-07-28 00:48:50 +0900

    chore(Circle): update circle logic

--- src/components/Circle.vue
+++ src/components/Circle.vue
@@ -1,66 +1,37 @@
-<template></template>
+<script lang="ts" setup>
+import { ref, inject, onMounted, onUnmounted, toRefs } from "vue";
+import { addEventCircle } from "@/composables/useEvent";
+import { UI_EVENT_CIRCLE } from "@/assets/event";
+import { MAPS_INSTANCE } from "@/config/keys";
 
-<script lang="ts">
-import {
-  defineComponent,
-  PropType,
-  inject,
-  ref,
-  toRefs,
-  onMounted,
-  onUnmounted,
-  watchEffect,
-} from "vue";
-import { naverMapObject } from "../injectionKeys";
-import { UI_EVENT_CIRCLE, addEventCircle } from "../utils";
+const props = defineProps<{
+  latitude: number;
+  longitude: number;
+  radius?: number;
+  options?: naver.maps.CircleOptions;
+}>();
+const emits = defineEmits(["onLoad", ...UI_EVENT_CIRCLE]);
 
-export default defineComponent({
-  name: "Circle",
-  emits: [...UI_EVENT_CIRCLE, "onLoad"],
-  props: {
-    latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true },
-    radius: { type: Number, default: 0 },
-    options: {
-      type: Object as PropType<naver.maps.CircleOptions>,
-      default: {},
-    },
-  },
-  setup: (props, { emit }) => {
-    const map = inject(naverMapObject)!;
-    const circle = ref<naver.maps.Circle>();
-    const { latitude, longitude, radius, options } = toRefs(props);
+const { latitude, longitude, radius, options } = toRefs(props);
+const map = inject(MAPS_INSTANCE)!;
+const circle = ref<naver.maps.Circle>();
 
-    const createCircle = () => {
-      circle.value = new window.naver.maps.Circle(
-        Object.assign(
-          {
-            map: map.value!,
-            center: new window.naver.maps.LatLng(
-              latitude.value,
-              longitude.value
-            ),
-            radius: radius.value,
-          },
-          options.value
-        )
-      );
+const getCircleInstance = () => {
+  const circleOption = {
+    map: map.value,
+    center: new window.naver.maps.LatLng(latitude.value, longitude.value),
+    radius: radius?.value ?? 0,
+  };
+  circle.value = new window.naver.maps.Circle(
+    Object.assign(circleOption, options?.value ?? {})
+  );
 
-      addEventCircle(emit, circle.value);
-      emit("onLoad", circle.value);
-    };
-    const setCircle = (newOptions: naver.maps.CircleOptions) => {
-      circle.value!.setOptions(newOptions);
-    };
+  addEventCircle(emits, circle.value);
+  emits("onLoad", circle.value);
+};
 
-    onMounted(() => {
-      watchEffect(() => {
-        const newOptions = options.value;
-        if (!map.value) return;
-        circle.value ? setCircle(newOptions) : createCircle();
-      });
-    });
-    onUnmounted(() => circle.value!.setMap(null));
-  },
-});
+onMounted(() => getCircleInstance());
+onUnmounted(() => circle.value!.setMap(null));
 </script>
+
+<template></template>
