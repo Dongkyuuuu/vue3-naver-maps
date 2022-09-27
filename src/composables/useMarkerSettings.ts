a971d6b7a8af54185264203c5d3404ed3d8a5e71
dongkyuuuu <desktop@kakao.com>
2022-09-27 13:36:59 +0900

    fix(Marker): HtmlIcon children nodes issue
    * set default icon if marker no has children node

--- src/composables/useMarkerSettings.ts
+++ src/composables/useMarkerSettings.ts
@@ -3,10 +3,7 @@ export interface HtmlIcon {
   anchor?: naver.maps.Point | naver.maps.PointLiteral | naver.maps.Position;
 }
 
-export const getIcon = (
-  icon?: string | HTMLElement,
-  iconOptions?: HtmlIcon
-) => {
-  if (!icon) return undefined; // default marker icon
+export const getIcon = (icon?: HTMLElement, iconOptions?: HtmlIcon) => {
+  if (!icon?.childElementCount) return undefined; // default marker icon
   return Object.assign({ content: icon }, iconOptions ?? {});
 };
